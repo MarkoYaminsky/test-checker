f052bccc61805d8e970f3d5403a98d6740fa3c19
Marko Yaminsky <marko.yaminskiy03@gmail.com>
2025-01-19 15:55:30 +0200

    Clean up unnecessary stuff

--- app/alembic/env.py
+++ app/alembic/env.py
@@ -5,6 +5,7 @@ from sqlalchemy import engine_from_config, pool
 
 from app.core.config import settings
 from app.core.db import Base
+from app.users.models import User  # noqa
 
 config = context.config
 
--- app/chat/api/http_handlers.py
+++
@@ -1,3 +0,0 @@
-from fastapi import APIRouter
-
-chat_http_router = APIRouter()
