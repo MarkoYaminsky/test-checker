0520a4b7ea5e1c4d08c4cb09abe824587429a4f4
Marko Yaminsky <marko.yaminskiy03@gmail.com>
2025-05-12 10:55:24 +0300

    Allow all CORS origins

--- app/core/main.py
+++ app/core/main.py
@@ -1,9 +1,18 @@
 from fastapi import FastAPI
+from starlette.middleware.cors import CORSMiddleware
 
 from app.student_tests.handlers import student_tests_router
 from app.users.handlers import users_router
 
 app = FastAPI()
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 app.include_router(users_router, prefix="/users")
 app.include_router(student_tests_router, prefix="/tests")
